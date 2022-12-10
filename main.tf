90856d6c3ab0c0dab6ae5542fbef8a71228009f1
kaustavbagchi <kaustavbagchi2@gmail.com>
2022-12-10 04:22:47 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwreker/docker"
+      version = "2.23.1"
+    }
+  }
+}
+provider "docker" {
+  #configuration options
+  host = "unix:///var/run/docker.sock"
+}
+resource "docker_image" "ubuntu" {
+  name = var.ubuntu_image
+}
+resource "docker_container" "ubuntu" {
+  name = var.container_name
+  image = docker_image.ubuntu.image_id
+  must_run = true
+  command = [
+    "tail",
+    "-f",
+    "/dev/null"
+  ]
+}
